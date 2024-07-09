3769528b70e7c9cc247f2a9f05b22e9528550a1c
ruveyda7979 <142754248+ruveyda7979@users.noreply.github.com>
2024-07-10 01:38:03 +0300

    updateee1

---
+++ Entities/Concrete/ProjectFile.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class ProjectFile : IEntity
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public string FileName { get; set; }
+        public string FileWay { get; set; }
+        public byte[] FileContent { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
