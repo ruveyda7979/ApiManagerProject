edf9067b6c57cb121160fcf3957541c90c5e4532
ruveyda7979 <142754248+ruveyda7979@users.noreply.github.com>
2024-07-26 15:30:57 +0300

    updateeee

---
+++ Web/Controllers/ProjectController.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IProjectService _projectService;
+
+
+        public ProjectController(IProjectService projectService)
+        {
+            _projectService = projectService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            var projects = await _projectService.GetAllProjectAsync();
+            return Ok(projects);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProjectById(int id)
+        {
+            var project = await _projectService.GetProjectByIdAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProject(Project project)
+        {
+            await _projectService.AddProjectAsync(project);
+            return CreatedAtAction(nameof(GetProjectById), new { id = project.Id }, project);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProject(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingProject = await _projectService.GetProjectByIdAsync(id);
+            if (existingProject == null)
+            {
+                return NotFound();
+            }
+
+            _projectService.UpdateProject(project);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var project = await _projectService.GetProjectByIdAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _projectService.DeleteProject( project);
+            return NoContent();
+        }
+    }
+}
